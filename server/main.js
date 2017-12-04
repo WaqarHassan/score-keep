d8f82dd2baeb6ebc37cf98b2775492387cee8cc9
waqar-vf <waqar.hassan@virtualforce.io>
2017-12-04 15:41:02 +0500

    mongo and mini mongo

--- server/main.js
+++ server/main.js
@@ -1,5 +1,10 @@
-import { Meteor } from 'meteor/meteor';
+import {Players} from './../imports/api/players';
+import  {Meteor} from 'meteor/meteor';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.startup(function () {
+    Players.insert({
+       name: "waqar",
+       score: 3
+    });
+    console.log(Players.find().fetch());
 });
