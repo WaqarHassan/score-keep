f8d040bda6fdeff4005645a3aabd9b54b4c44c8d
waqar-vf <waqar.hassan@virtualforce.io>
2017-12-04 15:19:42 +0500

    sb planning and rendering data

--- client/main.js
+++ client/main.js
@@ -2,8 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
+const players = [
+    {
+        _id: '1',
+        name: 'Lauren',
+        score: 30
+    },
+    {
+        _id: '2',
+        name: 'Max',
+        score: 30
+    }
+        ]
+ const renderPlayers = function (playersList) {
+     return playersList.map((player) => {
+         return <p key = {player._id}> {player.name} has {player.score} point(s).  </p>;
+     });
+ };
 Meteor.startup( function () {
-    let name = "Waqar "
-    let jsx = <p> This is from main by {name}. </p>;
+    let name = "Waqar ";
+    let title = "Score Keep";
+    let jsx = (
+        <div>
+        <h1> {title}</h1>
+            <p> Hello {name} </p>
+            <p> this is my second p. </p>
+            {renderPlayers(players)}
+        </div>
+
+    );
     ReactDOM.render(jsx, document.getElementById('root'));
 });
